b684ebd541f3c8c2c926a844590715a63fa19354
Nithin <vooturinithinkumar@gmail.com>
2025-12-28 16:51:50 +0530

    update end responses

--- maintenance/src/constants/responseInfo.js
+++ maintenance/src/constants/responseInfo.js
@@ -10,7 +10,13 @@ const info = {
   MNS_DATA_FETCHED_OK: "Maintenance Vehicles data fetched successfully from the vehicle client-VehicleMicroService",
   MNS_DATA_FETCHED_NOT_OK : "Error fetching details of vehicles under maintenance from the vehicle client",
   DISTANCE_TRAVELLED_THRESHOLD:5000,
-  VEHICLES_MNTCE: "Currently, there are no vehicles scheduled for maintenance.”"
+  VEHICLES_MNTCE: "Currently, there are no vehicles scheduled for maintenance.",
+  QUEUE_CONN_SUCESS: "Queue connection successfull",
+  EVENT_SUBNAME:"Maintenance",
+  EVENT_TYPE:"MAINTENANCE_DUE",
+  MAINTENANCE_DATA_SENT:"Vehicles maintenance data has sent into NOTIFICATION_QUEUE  for further process",
+  MAINTENANCE_DATA_PUBLISH:"Vehicles maintenance data published into NOTIFICATION_QUEUE successfully",
+  QUEUE_CON_CLOSE: "Queue Connection closed",
  
 }
 module.exports = info;
