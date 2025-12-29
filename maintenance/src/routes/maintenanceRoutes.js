81305d4ac0e65cc459db09bd382548f2a1ddbbda
Nithin <vooturinithinkumar@gmail.com>
2025-12-29 15:05:52 +0530

    update role

--- maintenance/src/routes/maintenanceRoutes.js
+++ maintenance/src/routes/maintenanceRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const {authorizeRole} = require('../middleware/authorizeUserRole');
 
 
-router.get('/maintenance-check', authorizeRole(["fleet_manager"]),mncController.viewMaintenanceVehicles);
+router.get('/maintenance-check', authorizeRole(["admin"]),mncController.viewMaintenanceVehicles);
 
 
 module.exports = router;
